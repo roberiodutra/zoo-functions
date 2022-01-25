5dfe1d3dd870ccdf58b1d4072f20dfebc7abd32b
Robério Almeida <dev.roberio@gmail.com>
2022-01-25 19:02:27 -0300

    lint 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // Busca pelo employee e armazema na variavel.
-  const find = data.employees.find((elem) => elem.firstName === employeeName || elem.lastName === employeeName);
-  
+  // Busca pelo employee e armazema seu obj na variavel, por fim retorna o object armazenado.
+  const find = data.employees.find((elem) =>
+  elem.firstName === employeeName || elem.lastName === employeeName);
   return employeeName ? find : {};
 }
 
