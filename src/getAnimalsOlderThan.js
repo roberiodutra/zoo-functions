dc7a08a492d1fa2c3b55e5e5f32c5e147a6f3b74
Robério Almeida <dev.roberio@gmail.com>
2022-01-25 18:32:22 -0300

    get animals older than, ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // Busca a specie utilizando o find.
+  const findSpecie = data.species.find((elem) => elem.name === animal);
+  // Mapeia a idade de todos os animais da specie encontrada acima e coloca em um array.
+  const mapAge = findSpecie.residents.map((elem) => elem.age);
+  // Compara se cada elemento do array acima, é maior ou igual ao parametro age.
+  const isAge = mapAge.every((elem) => elem >= age);
+  return isAge;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
+  // Um spread no parametro recebido, mapeia os valores e busca(find) nos dados(zoo_data) o animal que é compativel com o ID recebido pelo map.
   const animals = ids.map((elem) => data.species.find((specie) => elem === specie.id));
   return animals;
 }
