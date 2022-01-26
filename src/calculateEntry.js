296a9c83c42962dfc294f56d0debb8db7b37a016
Robério Almeida <dev.roberio@gmail.com>
2022-01-26 15:39:23 -0300

    calculate entry 1/6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+const toArray = (elem) => {
+  const number = [];
+  elem.map(function(obj) {
+    return Object.keys(obj).map(function(key) {
+        if (typeof obj[key] === 'number') {
+          number.push(obj[key]);
+        }
+    });
+  });
+  return number;
+}
+
 function countEntrants(entrants) {
-  // seu código aquii
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  toArray(entrants).forEach((elem) => {
+    if (elem < 18) {
+      child += 1;
+    } else if (elem >= 18 && elem < 50) {
+      adult += 1;
+    } else {
+      senior += 1;
+    }
+  });
+  return { adult, child, senior };
 }
 
 function calculateEntry(entrants) {
