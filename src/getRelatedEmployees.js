52a903f50b4fdb85c3df292df57e31c5950da6b7
Robério Almeida <dev.roberio@gmail.com>
2022-01-25 22:34:06 -0300

    get related employees, ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // Busca se o ID está contido como manager de algum employee
+  const bool = data.employees.find((elem) =>
+    elem.managers.includes(id));
+  return bool ? true : false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  let arr = [];
+  // Filtra todos os objetos que incluam como manager o ID fornecido
+  const filter = data.employees.filter((elem) =>
+    elem.managers.includes(managerId));
+  // Mapeia e armazena em array os nomes dos employees salvos em $filter
+  filter.map((elem) =>
+    arr.push(`${elem.firstName} ${elem.lastName}`));
+
+  // Checa se a função de fora retorna falso com o ID fornecido e gera um erro se falsa
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return arr;
 }
 
 module.exports = { isManager, getRelatedEmployees };
