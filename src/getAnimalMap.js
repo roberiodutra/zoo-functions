7d70efa841529ea65f8e7dcdbe32e8401b3510ae
Robério Almeida <dev.roberio@gmail.com>
2022-01-27 19:51:33 -0300

    get animal map 1/7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  const area = ['NE', 'NW', 'SE', 'SW'];
+  const local = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  data.species.forEach((elem) => {
+    for(let i = 0; i < area.length; i += 1) {
+      if (elem.location === area[i]) {
+        local[area[i]].push(elem.name);
+      }
+    }
+  });
+  return local;
 }
+
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
