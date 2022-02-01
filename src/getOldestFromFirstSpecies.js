6475d885c91c91201cf1f1c55485a8aa924d2916
Robério Almeida <dev.roberio@gmail.com>
2022-02-01 16:07:35 -0300

    oldest from first species, ok

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
+const { employees, species } = data;
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const elder = (specieId) => {
+  let obj = {};
+  species.forEach((elem) => {
+    if (elem.id === specieId) {
+      elem.residents.reduce((acc, curr) => {
+        if (curr.age > acc) {
+          acc = curr.age
+          obj = curr;
+        }
+        return acc;
+      }, 0);
+    }
+  });
+  return Object.values(obj);
+};
+
+function getOldestFromFirstSpecies(param) {
+  let animalId = '';
+  employees.forEach((elem) => {
+    if (elem.id === param) {
+      animalId = elem.responsibleFor[0];
+    }
+  });
+  return elder(animalId);
 }
 
 module.exports = getOldestFromFirstSpecies;
